8c7d704c89e03ff599195ac5119c1e04e671b244
Kasper de Graaff <kasperdegraaff1997@gmail.com>
2022-01-12 12:17:35 +0100

    create-react-app install tailwind

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col bg-sky-900 w-screen h-screen text-white">
+      <h1 className=''>Hello Solana </h1>
+    </div>
+  );
+}
+
+export default App;
