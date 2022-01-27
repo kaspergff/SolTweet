58226a1368ee8fb91de45e0b041cf9d1b5de815a
Kasper de Graaff <kasperdegraaff1997@gmail.com>
2022-01-27 13:44:59 +0100

    done for now

--- app/components/PostForm/PostForm.tsx
+++ app/components/PostForm/PostForm.tsx
@@ -69,7 +69,7 @@ export default function PostForm(props: Props) {
           <button
             type="submit"
             className="py-2 px-4  bg-purple-600 hover:bg-purple-800 focus:ring-purple-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-            Upload
+            Submit
           </button>
         </div>
       </div>
